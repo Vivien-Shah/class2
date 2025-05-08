def859722c121d78dc0bacb3c6691f45f72a876c
Vivien-Shah <hfsthane7787@gmail.com>
2025-05-08 11:17:37 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,22 @@
+#print a sentence
+print("Welcome to the World")
+# print a number
+print (25)
+print("My name is Vivien.\n \n I am 11 years old")
+print("Hello",5)
+print("Welcome",end="$")
+print()
+#activity 2-variable
+x="John"
+print(x)
+name="laptop" 
+print(name)
+number=26
+print(number)
+#user input
+a=input("What is your name?")
+print(a)
+
+#printing keywords
+import keyword
+print(keyword.kwlist)
