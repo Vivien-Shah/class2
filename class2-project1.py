a3d710312b4c473c8eb9546a640b84fbca5f630e
Vivien-Shah <hfsthane7787@gmail.com>
2025-05-11 16:24:22 +0530

    Add files via upload

---
+++ class2-project1.py
@@ -0,0 +1,12 @@
+# Program to store 5 birthdates in variables
+b_date1 = input("What is the 1st birthday date?")
+b_date2 = input("What is the 2nd birthday date?")
+b_date3 = input("What is the 3rd birthday date?")
+b_date4 = input("What is the 4th birthday date?")
+b_date5 = input("What is the 5th birthday date?")
+
+print("\n",b_date1,"\n")
+print(b_date2,"\n") 
+print(b_date3, "\n") 
+print(b_date4, "\n")
+print(b_date5, "\n") 
